3fd44480965d3911e999f0abe69b0e2111396abb
fitzgibbonjack <63608338+fitzgibbonjack@users.noreply.github.com>
2022-02-22 01:31:32 +0000

    Updated about copy

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,29 +3,31 @@ import "./About.scss";
 import "../Button/Button.scss";
 
 export default function About() {
-  return (
-    <section className="about" id="about">
-      <div className="container">
-        <h2 className="about__title">&#128075; - I'm Jack Fitzgibbon</h2>
-        <p className="about__para">
-          Curious by nature, and business minded. I'm a self taught, 21 year old
-          creative developer with a passion for creating things that live on the
-          internet.
-        </p>
-        <p className="about__para">
-          Leveraging the latest and superlative web technologies to create an
-          engaging and refined web experience, I can take an idea from design
-          and ideation, to deployment.
-        </p>
-        <p className="about__para">
-          In my spare time when I'm not coding, you'll probably find me playing
-          Apex Legends, or working on my latest side project.
-        </p>
+	return (
+		<section className="about" id="about">
+			<div className="container">
+				<h2 className="about__title">
+					&#128075; - I'm Jack Fitzgibbon
+				</h2>
+				<p className="about__para">
+					Curious by nature, and business minded. I'm a self taught,
+					21 year old front end developer with a passion for creating
+					things that live on the internet.
+				</p>
+				<p className="about__para">
+					Leveraging the latest and superlative web technologies to
+					create an engaging and refined experience.
+				</p>
+				<p className="about__para">
+					In my spare time when I'm not coding, you'll probably find
+					me playing Apex Legends, or working on my latest side
+					project.
+				</p>
 
-        <a className="button--primary about__button" href="/about">
-          more about me
-        </a>
-      </div>
-    </section>
-  );
+				<a className="button--primary about__button" href="/about">
+					more about me
+				</a>
+			</div>
+		</section>
+	);
 }
